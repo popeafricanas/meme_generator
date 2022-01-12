b8a8d0861c080b6671a9b277be48acb97020fd17
John Tarry <john.tarry@centricsoftware.com>
2022-01-12 15:49:18 -0500

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return(
         <header className="header">
             <img className="header--image" src={logo} alt="logo"/>
-            <h2 className="header--title">MemeGenerator</h2>
+            <h2 className="header--title">.: MemeGenerator :.</h2>
         </header>
     )
 }
